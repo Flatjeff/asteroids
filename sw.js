b2f972702076c43e35902fb876c2e35a9a76981b
Flatjeff <41294597+Flatjeff@users.noreply.github.com>
2026-06-13 21:50:18 +0200

    PWA: cache asteroids-v75

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'asteroids-v1';
+const CACHE = 'asteroids-v75';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
