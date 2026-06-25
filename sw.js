bb24d10d6ccd4eab0dd39a7f6388b304072110fa
Flatjeff <41294597+Flatjeff@users.noreply.github.com>
2026-06-25 22:42:44 +0200

    v78e: cache update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'asteroids-v78d';
+const CACHE = 'asteroids-v78e';
 const ASSETS = ['/asteroids/', '/asteroids/index.html', '/asteroids/manifest.json', '/asteroids/icon-192.png', '/asteroids/icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
