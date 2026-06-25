93a77639e6f3bb76a938891c4c912efa2ec051ac
Flatjeff <41294597+Flatjeff@users.noreply.github.com>
2026-06-25 22:38:16 +0200

    v78c: cache update

--- sw.js
+++ sw.js
@@ -1,24 +1,5 @@
-const CACHE = 'asteroids-v78';
+const CACHE = 'asteroids-v78c';
 const ASSETS = ['/asteroids/', '/asteroids/index.html', '/asteroids/manifest.json', '/asteroids/icon-192.png', '/asteroids/icon-512.png'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
-      const clone = res.clone();
-      caches.open(CACHE).then(c => c.put(e.request, clone));
-      return res;
-    }).catch(() => caches.match('/asteroids/')))
-  );
-});
+self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))); self.clients.claim(); });
+self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request).then(res => { const clone = res.clone(); caches.open(CACHE).then(c => c.put(e.request, clone)); return res; }).catch(() => caches.match('/asteroids/')))); });
